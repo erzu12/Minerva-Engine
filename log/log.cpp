abcb353a4c8fe8ade4cf539faa947fdd75026ad7
erzu12 <jonas.fink@gmx.ch>
2020-11-13 14:09:17 +0100

    Init

---
+++ log/log.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "log.h"
+
+void log(char* output) {
+	std::cout << output << std::endl;
+}
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <log.h>
+
+int main()
+{
+    log("hello");
+    return 0;
+}
